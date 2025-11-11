719712609473962df0ae2e722b237ef7f739b69d
ClementReverbel <reverbel.cle@gmail.com>
2025-11-11 15:58:38 +0100

    Some improvements

--- main.py
+++ main.py
@@ -16,8 +16,19 @@ async def status(ctx):
     await ctx.send("[🟢] En ligne")
     
 @bot.command()
-async def pray(ctx):
-    print("ouiii")
-    #await ctx.send(PP.prayer_adjust_time())
+async def planning(ctx):
+    prayer_list=PP.prayer_adjust_time()
+    msg = "Vos prières auront lieu aujourd'hui :"
+    for prayer_time in prayer_list:
+        msg += f"\n- {prayer_time}"
+    await ctx.send(msg)
+    
+@bot.command()
+async def who(ctx):
+    await ctx.send("Tu es " + ctx.author.mention)
+    
+@bot.command()
+async def register(ctx,ville,decalage):
+    await ctx.send("Fonction d'enregistrement non encore implémentée.")
 
 bot.run(environ.get("DISCORD_BOT_TOKEN"))   
